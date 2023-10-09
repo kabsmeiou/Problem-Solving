6cf863ffdf6376d1499ecea096bfcb914ce6d0a1
kabsmeiou <94173937+kabsmeiou@users.noreply.github.com>
2023-10-09 13:44:23 +0800

    binarydeque bs added

--- Random/binarydeque.cpp
+++ Random/binarydeque.cpp
@@ -6,17 +6,30 @@ int main() {
     while (T--) {
         int n, s;
         std::cin >> n >> s;
-        std::vector<int> a(n);
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
+        std::vector<int> a(n + 1, 0), pref(n + 1);
+        for (int i = 1; i <= n; i++) {
             std::cin >> a[i];
-            sum += a[i];
+            pref[i] = pref[i - 1] + a[i];
         }
-        if (sum < s) {
+        if (pref[n] < s) {
             std::cout << -1 << '\n';
             continue;
         }
-        int cur = 0, mx = -1;
+        int ans = 1e9;
+        for (int i = 1; i <= n; i++) {
+            int l = i, r = n;
+            while (l <= r) {
+                int mid = (l + r) >> 1;
+                if (pref[mid] - pref[i - 1] > s) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            ans = std::min(ans, (n - r) + (i - 1));
+        }
+        std::cout << ans << '\n';
+        /*int cur = 0, mx = -1;
         for (int i = 0, j = 0; i < n; i++) {
             while (j < n && cur + a[j] <= s) { //get biggest window with sum == s
                 cur += a[j];
@@ -28,5 +41,6 @@ int main() {
             cur -= a[i];
         }
         std::cout << n - mx << '\n';
+        */
     }
 }
