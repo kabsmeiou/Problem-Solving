2ed2cb41ebeff4dd38f7c2092d9890a58f6eaadf
hopskiprabbit <94173937+kabsmeiou@users.noreply.github.com>
2024-07-10 17:01:36 +0800

    update

--- Rounds/Round 915(Div 2)/C.cpp
+++ Rounds/Round 915(Div 2)/C.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int T;
-    cin >> T;
-    while (T--) {
-        int n;
-        string s;
-        cin >> n >> s;
-        //what the hell is this problem??
-        if (s[1] < s[0] && s[n - 2] < s[n - 1]) {
-            cout << -1 << '\n';
-            continue;
-        }
-        int ans = 0;
-        for (int i = 1; i < n; i++) {
-            if (s[i] < s[i - 1]) ans++;
-        }
-        cout << ans << '\n';
+signed main () {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  int T;
+  cin >> T;
+  while (T--) {
+    int n;
+    string s;
+    cin >> n >> s;
+    vector<int> subsequence;
+    for (int i = 0; i < n; i += 1) {
+      while (!subsequence.empty() && s[subsequence.back()] < s[i]) subsequence.pop_back();
+      subsequence.push_back(i);
     }
+    int cnt = 0;
+    while (cnt < subsequence.size() && s[subsequence[0]] == s[subsequence[cnt]]) cnt += 1;   
+    int k = subsequence.size();
+    for (int i = 0; i < k - i - 1; i += 1) swap(s[subsequence[i]], s[subsequence[k - i - 1]]);
+    cout << (is_sorted(s.begin(), s.end()) ? k - cnt : -1) << '\n';
+  }
 }
